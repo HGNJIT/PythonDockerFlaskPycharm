9c8cef321d22a70a917e96d86bf92dccced018ba
Gil <heg2@njit.edu>
2021-04-30 20:24:57 -0400

    Flask 1

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, Response, redirect
 from flask import render_template
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
+from flask import Flask
 
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
@@ -15,6 +16,12 @@ app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'citiesData'
 mysql.init_app(app)
 
+# Create Flask's `app` object
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 @app.route('/', methods=['GET'])
 def index():

d162a51464b6f742d8224c1d85e97c3ff57ee0b0
Gil <heg2@njit.edu>
2021-04-30 20:41:24 -0400

    Flask 3

--- app/app.py
+++ app/app.py
@@ -4,8 +4,7 @@ from flask import Flask, request, Response, redirect
 from flask import render_template
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
-from flask import Flask
-from logic import square_of_number_plus_nine
+from flask import Flask, make_response, request, jsonify
 
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
@@ -18,12 +17,15 @@ app.config['MYSQL_DATABASE_DB'] = 'citiesData'
 mysql.init_app(app)
 
 # Create Flask's `app` object
-app = Flask(__name__,
-    __name__,
-    instance_relative_config=False,
-    template_folder="templates",
-    static_folder="static"
-)
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello():
+    if request.method != 'GET':
+        return make_response('Malformed request', 400)
+    my_dict = {'key': 'dictionary value'}
+    headers = {"Content-Type": "application/json"}
+    return make_response(jsonify(my_dict), 200, headers)
 
 @app.route("/")
 def hello():
